5fca03b696fe28021f65d48105a043b830723387
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-09-10 23:17:54 +0900

    habit-traker 프로젝트 생성

---
+++ reart-basic/habit-tracker/src/app.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./app.css";
+
+function App() {
+  const name = "wonsik";
+  return (
+    <React.Fragment>
+      <h1>Hello :) {name}</h1>
+      <h1>Hello</h1>
+    </React.Fragment>
+  );
+}
+
+export default App;

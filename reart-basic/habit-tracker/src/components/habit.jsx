93955d5e173bc3de8b51f5bfd15ebbd975140a67
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-09-10 23:40:36 +0900

    Habit 컴포넌트 State 적용

--- reart-basic/habit-tracker/src/components/habit.jsx
+++ reart-basic/habit-tracker/src/components/habit.jsx
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
+  state = {
+    count: 0,
+  };
+  handleIncreament = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+  handleDecreamemt = () => {
+    const count = this.state.count - 1;
+    this.setState({ count: count < 0 ? 0 : count });
+  };
   render() {
     return (
       <li className="habit">
         <span className="habit-name">Reading</span>
-        <span className="habit-count">8</span>
-        <button className="habit-button habit-increase">
+        <span className="habit-count">{this.state.count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncreament}
+        >
           <i className="fas fa-plus-square"></i>
         </button>
-        <button className="habit-button habit-decrease">
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecreamemt}
+        >
           <i className="fas fa-minus-square"></i>
         </button>
         <button className="habit-button habit-delete">

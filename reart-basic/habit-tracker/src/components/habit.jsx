98f3356ebb6b57dd30671953686061d9bc5ca8aa
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-09-11 23:59:16 +0900

    habits 컴포넌트 생성

--- reart-basic/habit-tracker/src/components/habit.jsx
+++ reart-basic/habit-tracker/src/components/habit.jsx
@@ -12,10 +12,11 @@ class Habit extends Component {
     this.setState({ count: count < 0 ? 0 : count });
   };
   render() {
+    const { name, count } = this.props.habit;
     return (
       <li className="habit">
-        <span className="habit-name">Reading</span>
-        <span className="habit-count">{this.state.count}</span>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
         <button
           className="habit-button habit-increase"
           onClick={this.handleIncreament}

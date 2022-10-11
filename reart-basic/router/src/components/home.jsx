ce5b6e0c42250b8a8ef62e8688bc0d2c75bb50b7
wonsikgo <45756634+wonsikgo@users.noreply.github.com>
2022-10-11 23:58:48 +0900

    React Router 실습

---
+++ reart-basic/router/src/components/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <>
+      <h1>Home</h1>
+      <button>Go to Home</button>
+    </>
+  );
+};
+
+export default Home;
---
+++ reart-basic/router/src/components/profile.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Profile = (props) => {
+  return (
+    <>
+      <h1>Profile</h1>
+      <button>Go to Profile</button>
+    </>
+  );
+};
+
+export default Profile;
